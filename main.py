d3e0f3be3a4fe0270a487b57761dbf633f78f0cc
Dayber Alfonso <0036161sm@gmail.com>
2026-01-16 17:04:22 -0500

    simple starter code for sentiment analysis

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from textblob import TextBlob
+
+def analyze_sentiment(text):
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    if sentiment > 0:
+        return "Positive"
+    elif sentiment < 0:
+        return "Negative"
+    else:
+        return "Neutral"
+
+
+# Test example
+post = "I love the weather today"
+print(analyze_sentiment(post))
